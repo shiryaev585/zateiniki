2d3a8706cd5fd74fbf39723ecc732001f7102c8b
@shiryaev1707 <shiryaev1707@gmail.com>
2024-05-12 16:52:15 +0300

    edited video content

--- pages/performances/index.vue
+++ pages/performances/index.vue
@@ -12,9 +12,9 @@ import VideoBlock from '~/components/blocks/VideoBlock.vue';
 const footerStore = useFooterStore();
 
 const videos = reactive([
-    { src: 'https://www.youtube.com/embed/lr1rYnUubpQ?si=3DteLTB1yGJjVgbX', img: '/images/index/intro_1.jpg' },
-    { src: 'https://www.youtube.com/embed/hMAfJDb6NF8?si=P70Cee-vapblMfUu', img: '/images/index/intro_2.jpg' },
-    { src: 'https://www.youtube.com/embed/hMAfJDb6NF8?si=P70Cee-vapblMfUu', img: '/images/index/intro_3.jpg' },
+    { src: 'https://www.youtube.com/embed/iNXNO2V0eoU?si=q69DYjhrwq1_I9ri', img: '/images/index/intro_1.jpg' },
+    { src: 'https://www.youtube.com/embed/xWVhf8s60eI?si=9DOz9eWUhzZbVeJJ', img: '/images/index/intro_2.jpg' },
+    { src: 'https://www.youtube.com/embed/LlVVv86XCmM?si=dTHgAsUFqo1LlKA3', img: '/images/index/intro_3.jpg' },
 ]);
 
 onMounted(() => {
