c582db52178451bb57b663e09c90cfa7a5ed0046
@shiryaev1707 <shiryaev1707@gmail.com>
2024-10-27 10:00:51 +0300

    update performances page

--- pages/performances/index.vue
+++ pages/performances/index.vue
@@ -2,7 +2,7 @@
     <div class="performances-page">
         <intro-block title="Спектакли" src="/images/index/intro_2.webp" class="block" />
         <content-block :content="videos" with-frame class="block" />
-        <photo-block :photos="photos" title="Наши выступления и репетиции" class="block" />
+        <photo-block :photos="performances" title="Наши выступления и репетиции" class="block" />
     </div>
 </template>
 <script setup>
@@ -13,8 +13,16 @@ import PhotoBlock from '~/components/blocks/PhotoBlock.vue';
 
 const footerStore = useFooterStore();
 
-const videos = [];
-const photos = [];
+const videos = [{
+    source_url: '/images/index/intro_1.webp',
+    video_src: 'https://www.youtube.com/embed/iNXNO2V0eoU?si=q69DYjhrwq1_I9ri'
+},{
+    source_url: '/images/index/intro_2.webp',
+    video_src: 'https://www.youtube.com/embed/xWVhf8s60eI?si=9DOz9eWUhzZbVeJJ'
+},{
+    source_url: '/images/index/intro_3.webp',
+    video_src: 'https://www.youtube.com/embed/LlVVv86XCmM?si=dTHgAsUFqo1LlKA3'
+}];
 
 useHead({
     title: 'Затейники - Спектакли',
@@ -22,6 +30,9 @@ useHead({
         { name: 'description', content: 'Детская театральная студия в Марьино, спектакли, выступления, репетиции, благотворительность' }
     ],
 });
+
+const { data: media } = await useApi('/media/', { query: { per_page: 100 } });
+const performances = computed(() => media.value?.filter((item) => item?.link?.includes('performances')));
 
 onMounted(() => {
     footerStore.setFooter(true);
